0b77cec6d631da9023357ada60c8512b33c470fa
Vitali Sarayeu <vitalisarayeu@protonmail.com>
2019-04-23 21:19:32 +0300

    New term window as bg process

--- .scripts/newterm.sh
+++ .scripts/newterm.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-echo $(pwd)
+#echo $(pwd)
 
-echo $@
+#echo $@
 
 # urxvt -cd $(pwd) -e bash -i -c ""$@"; bash"
 # urxvt -cd $(pwd) --hold -e "bash" -i -c "$(echo "$@")"
 
-bash -c "$(echo " urxvt -cd $(pwd) --hold -e "bash" -i -c \"$(echo "$@")\"")"
-
+nohup bash -c "$(echo " urxvt -cd $(pwd) --hold -e "bash" -i -c \"$(echo "$@")\"")" </dev/null >/dev/null 2>&1 &
 
