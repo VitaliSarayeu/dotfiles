b525d8a85e8a6e7ef26dc80088421fba04a69d7e
Vitali Sarayeu <vitalisarayeu@protonmail.com>
2025-02-20 23:17:12 +0300

    Added keybind for zettel

--- .config/nvim/lua/plugins/zettel.lua
+++ .config/nvim/lua/plugins/zettel.lua
@@ -1,6 +1,10 @@
 -- https://github.com/nvim-telekasten/telekasten.nvim
 return {
     'renerocksai/telekasten.nvim',
-    dependencies = {'nvim-telescope/telescope.nvim'}
+    dependencies = {'nvim-telescope/telescope.nvim'},
+	keys = {
+    	{ "<leader>zt", "<cmd>Telekasten<cr>", desc = "Telekazten notes" }
+	}
+
 }
 
