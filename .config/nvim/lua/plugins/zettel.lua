c9b4810d6b97d87d373c183d10da75865a062fd8
Vitaly Saraev <vitalysaraev@coherentsolutions.com>
2025-02-25 17:27:29 +0300

    Todo toggle for zettel

--- .config/nvim/lua/plugins/zettel.lua
+++ .config/nvim/lua/plugins/zettel.lua
@@ -3,7 +3,8 @@ return {
     'renerocksai/telekasten.nvim',
     dependencies = {'nvim-telescope/telescope.nvim'},
 	keys = {
-    	{ "<leader>zt", "<cmd>Telekasten<cr>", desc = "Telekazten notes" }
+    	{ "<leader>zt", "<cmd>Telekasten<cr>", desc = "Telekazten notes" },
+		{ "<leader>zd", "<cmd>Telekasten toggle_todo<cr>", desc = "Telekazten todo toggle" },
 	}
 
 }
